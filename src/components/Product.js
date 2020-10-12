fe9ad56dc53a0a1ac55dc809a397247203183753
Talha-Bayansar <talha.bayansar@hotmail.com>
2020-10-12 20:23:08 +0200

    product component done

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,8 +2,33 @@ import React from 'react'
 import styled from "@emotion/styled";
 
 const StyledProduct = styled.div`
-    background-color: #ececec;
+    background-color: #c1c1c1;
     box-shadow: 0px 0px 5px 2px black;
+    border-radius: 5px;
+
+    & > img{
+        width: 100%;
+        object-fit: contain;
+        border-radius: 5px 5px 0 0;
+    }
+
+    & > div{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-around;
+        margin-bottom: 1rem;
+
+        & > button{
+            background-color: #3e3e9f;
+            border: none;
+            border-radius: 3px;
+            padding: 5px 10px;
+            font-size: 1rem;
+            cursor: pointer;
+            outline: none;
+        }
+    }
 `;
 
 function Product(props) {
@@ -12,10 +37,10 @@ function Product(props) {
 
     return (
         <StyledProduct>
-            <img src={product.imageUrl} alt={product.title}/>
+            <img src={product.imgUrl} alt={product.title}/>
             <div>
                 <h2>{product.title}</h2>
-                <span>{product.price}&euro;</span>
+                <button>{product.price}&euro;</button>
             </div>
         </StyledProduct>
     )
--- src/data/products_data.js
+++ src/data/products_data.js
@@ -2,19 +2,19 @@ export const products_data = [
     {
         id: 0,
         title: "Playstation 4",
-        imgUrl: "",
+        imgUrl: "https://www.lifewire.com/thmb/J2Q6R7gQwpoIMN87_VUm7EqVoeQ=/1500x1500/filters:fill(auto,1)/1LW4154610HeroSquare-930a91489a594b1293a03b5d53359feb.jpg",
         price: 300,
     },
     {
         id: 1,
         title: "Laptop",
-        imgUrl: "",
+        imgUrl: "https://images.unsplash.com/photo-1561152820-340780bc049e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
         price: 600,
     },
     {
         id: 2,
-        title: "Nike Mercurial",
-        imgUrl: "",
+        title: "Nike Shoes",
+        imgUrl: "https://images.unsplash.com/photo-1573875133405-66562f496fa9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
         price: 60,
     },
 ]

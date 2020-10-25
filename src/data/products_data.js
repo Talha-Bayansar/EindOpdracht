473ca29a0427f0d9ac2215249664da987ba1a7fb
Talha-Bayansar <talha.bayansar@hotmail.com>
2020-10-25 19:49:54 +0100

    updated content

--- src/data/products_data.js
+++ src/data/products_data.js
@@ -28,20 +28,20 @@ export const products_data = [
   },
   {
     id: 3,
-    title: "Nike Shoes",
+    title: "Xbox Controller",
     imgUrl:
-      "https://images.unsplash.com/photo-1573875133405-66562f496fa9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
+      "https://images.unsplash.com/photo-1587843618590-dd2f6ea2ccf2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
     price: 60,
-    description: "Nike shoes. White with black Nike logo.",
-    category: "Shoes",
+    description: "Xbox controller black, Xbox One",
+    category: "Electronics",
   },
   {
     id: 4,
-    title: "Nike Shoes",
+    title: "Game Boy Color",
     imgUrl:
-      "https://images.unsplash.com/photo-1573875133405-66562f496fa9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
-    price: 60,
-    description: "Nike shoes. White with black Nike logo.",
-    category: "Shoes",
+      "https://images.unsplash.com/photo-1531525645387-7f14be1bdbbd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
+    price: 100,
+    description: "Old School Game Boy Color, yellow",
+    category: "Electronics",
   },
 ];

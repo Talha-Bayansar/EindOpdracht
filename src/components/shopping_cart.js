198fc33e91670001f8efc94bb1bc60867fa5d7bf
Talha-Bayansar <talha.bayansar@hotmail.com>
2020-10-25 20:18:27 +0100

    added shopping cart

--- src/components/shopping_cart.js
+++ src/components/shopping_cart.js
@@ -4,24 +4,7 @@ import ShoppingcartItem from "./shopping_cart_item";
 import { useCartContext } from "../contexts/cart_context";
 
 const StyledShoppingcart = styled.div`
-  height: 100vh;
-  width: 30vw;
-  position: fixed;
-  top: 0;
-  right: 0;
-  background-color: #3e3e9f;
   padding: 1rem;
-  transform: ${(props) =>
-    props.isActive ? "translateX(0%)" : "translateX(100%)"};
-  transition: transform 0.3s ease-in-out;
-  z-index: 5;
-
-  & button {
-    background-color: transparent;
-    font-size: 2rem;
-    border: none;
-    cursor: pointer;
-  }
 
   & h2 {
     margin-top: 3rem;
@@ -34,7 +17,6 @@ function Shoppingcart(props) {
 
   return (
     <StyledShoppingcart isActive={isCartActive}>
-      <button onClick={() => setIsCartActive(false)}>x</button>
       <div>
         {cart.length === 0 ? (
           <h2>Shopping cart is empty.</h2>

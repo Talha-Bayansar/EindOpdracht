f37aa72d53977a4b7327db0e11d6e92f55ccc118
Talha-Bayansar <talha.bayansar@hotmail.com>
2020-11-09 22:32:50 +0100

    error message styling

--- src/components/error.js
+++ src/components/error.js
@@ -1,7 +1,15 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const StyledError = styled.div`
+  background-color: #f0a7a7;
+  color: red;
+  padding: 5px 10px;
+  border-radius: 5px;
+`;
 
 function Error(props) {
-  return <>{props.children}</>;
+  return <StyledError>{props.children}</StyledError>;
 }
 
 export default Error;

d380d113295fc484918566d32a8c6defe2424e4c
Talha-Bayansar <talha.bayansar@hotmail.com>
2020-11-10 20:08:09 +0100

    products page done

--- src/pages/products_page.js
+++ src/pages/products_page.js
@@ -1,7 +1,22 @@
 import React from "react";
+import ContentProducts from "../components/content_products";
+import { useAllProductsContext } from "../contexts/all_products_context";
+import Product from "../components/product";
 
 function ProductsPage() {
-  return <div>Products</div>;
+  const { allProducts } = useAllProductsContext();
+
+  return (
+    <>
+      <ContentProducts title={"All Products"}>
+        {allProducts.length > 0 ? (
+          allProducts.map((p) => <Product key={p.id} product={p} />)
+        ) : (
+          <span>There aren't available products at the moment.</span>
+        )}
+      </ContentProducts>
+    </>
+  );
 }
 
 export default ProductsPage;
